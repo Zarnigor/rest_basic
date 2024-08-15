1323baee0e64256eb15308b8b66a209834604aa9
Zarnigor <zarnigorn1255@gmail.com>
2024-08-15 19:51:38 +0500

    filters

---
+++ apps/filters.py
@@ -0,0 +1,24 @@
+from django.db.models import Count
+from django_filters import FilterSet, BooleanFilter
+
+from apps.models import Category, Product
+
+
+class CategoryFilterSet(FilterSet):
+    class Meta:
+        model = Category
+        fields = ['name']
+
+
+class ProductFilterSet(FilterSet):
+    image = BooleanFilter(method="has_image")
+
+    class Meta:
+        model = Product
+        fields = []
+
+    def has_image(self, queryset, field, value):
+        if value:
+            return queryset.annotate(product_icount=Count('images')).filter(product_icount__gte=1)
+
+        return queryset
